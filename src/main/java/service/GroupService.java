e7e940b878dc28afaf7124535a155c207a0edfca
Lokesh Bisht <lokeshbisht.096@gmail.com>
2024-02-17 17:26:31 +0530

    feat: #5 Add feature to update group info

--- src/main/java/service/GroupService.java
+++ src/main/java/service/GroupService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface GroupService {
 
     void createGroup(List<User> users, String imageUri, String title, String description);
-    void updateGroupInfo(Long groupId);
-    void fetchGroup(Long groupId);
-    void deleteGroup(Long groupId);
+    void updateGroupInfo(String groupId, String imageUri, String title, String description);
+    void fetchGroup(String groupId);
+    void deleteGroup(String groupId);
 }
--- src/main/java/service/impl/GroupServiceImpl.java
+++ src/main/java/service/impl/GroupServiceImpl.java
@@ -1,5 +1,6 @@
 package service.impl;
 
+import exception.GroupNotFoundException;
 import models.Group;
 import models.User;
 import org.slf4j.Logger;
@@ -38,17 +39,26 @@ public class GroupServiceImpl implements GroupService {
     }
 
     @Override
-    public void updateGroupInfo(Long groupId) {
+    public void updateGroupInfo(String groupId, String imageUri, String title, String description) {
+        logger.info("Update group info for group {}", groupId);
+        if (groupRepository.findById(groupId).isEmpty()) {
+            logger.error("An error occurred while updating the group info.");
+            throw new GroupNotFoundException("Group with groupId " + groupId + "doesn't exist");
+        }
+        Group group = groupRepository.findById(groupId).get();
+        group.setImageURI(imageUri);
+        group.setTitle(title);
+        group.setDescription(description);
+        groupRepository.save(group);
+    }
+
+    @Override
+    public void fetchGroup(String groupId) {
 
     }
 
     @Override
-    public void fetchGroup(Long groupId) {
-
-    }
-
-    @Override
-    public void deleteGroup(Long groupId) {
+    public void deleteGroup(String groupId) {
 
     }
 }
